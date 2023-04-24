301f9d0ec343f96f9f4305c0de093d59f2fb75cc
vishnuSekhar <vishnusekhar96@gmail.com>
2023-04-24 13:10:14 +0530

    fix:add-provider

--- src/context/CartProvider.tsx
+++ src/context/CartProvider.tsx
@@ -1,3 +1,5 @@
+import { ReactElement, createContext, useMemo, useReducer } from "react";
+
 export type CartItemType = {
   sku: string;
   name: string;
@@ -33,16 +35,22 @@ const reducer = (
       if (!action.payload) {
         throw new Error("action.pay;oad missing in ADD");
       }
+
       const { sku, name, price } = action.payload;
+
       const filteredCart: CartItemType[] = state.cart.filter(
         (item) => item.sku !== sku
       );
+
       const itemExists: CartItemType | undefined = state.cart.find(
         (item) => item.sku === sku
       );
+
       const qty: number = itemExists ? itemExists.qty + 1 : 1;
+
       return { ...state, cart: [...filteredCart, { sku, name, price, qty }] };
     }
+
     case REDUCER_ACTION_TYPES.REMOVE: {
       if (!action.payload) {
         throw new Error("action.pay;oad missing in REMOVE");
@@ -53,6 +61,7 @@ const reducer = (
       );
       return { ...state, cart: [...filteredCart] };
     }
+
     case REDUCER_ACTION_TYPES.QUANTITY: {
       if (!action.payload) {
         throw new Error("action.pay;oad missing in QUANTITY");
@@ -79,3 +88,55 @@ const reducer = (
       throw new Error("Unidentified reducer action");
   }
 };
+
+const useCartContext = (initCartState: CartStateType) => {
+  const [state, dispatch] = useReducer(reducer, initCartState);
+  const REDUCER_ACTIONS = useMemo(() => {
+    return REDUCER_ACTION_TYPES;
+  }, []);
+  const totalItems = state.cart.reduce((previousValue, cartItem) => {
+    return previousValue + cartItem.qty;
+  }, 0);
+
+  const totalPrice = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(
+    state.cart.reduce((previosValue, cartItem) => {
+      return previosValue + cartItem.qty * cartItem.price;
+    }, 0)
+  );
+  const cart = state.cart.sort((a, b) => {
+    const itemA = Number(a.sku.slice(-4));
+    const itemB = Number(b.sku.slice(-4));
+    return itemA - itemB;
+  });
+
+  return { dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart };
+};
+
+export type UseCartContextType = ReturnType<typeof useCartContext>;
+const initCartContextState: UseCartContextType = {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  dispatch: () => {},
+  REDUCER_ACTIONS: REDUCER_ACTION_TYPES,
+  totalItems: 0,
+  totalPrice: "",
+  cart: [],
+};
+
+export const CartContext =
+  createContext<UseCartContextType>(initCartContextState);
+
+type ChildrenType = {
+  children?: ReactElement | ReactElement[];
+};
+
+export const CartProvider = ({ children }: ChildrenType): ReactElement => {
+  return (
+    <CartContext.Provider value={useCartContext(initCartState)}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+export default CartContext;
